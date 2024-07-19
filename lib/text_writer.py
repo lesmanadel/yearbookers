b7342865078194abc1a08e0c9a44627ef1940ac3
dennis lesmana <dennis.lesmana@vidio.com>
2024-07-19 11:49:54 +0700

    add text_writer()

---
+++ lib/text_writer.py
@@ -0,0 +1,12 @@
+def get_font_with_auto_adjusted_size(font, starting_size, boundaries, text):
+    font_size = starting_size
+    size = None
+
+    # adjusting font size to fit inside boundaries
+    while (size is None or size[0] > boundaries[2] - boundaries[0] or size[1] > boundaries[3] - boundaries[1]) and font_size > 0:
+        # font = ImageFont.truetype(font, font_size)
+        font = font.font_variant(size=font_size)
+        size = font.getsize_multiline(text)
+        font_size -= 1
+
+    return font
---
+++ tests/lib/text_writer_test.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from PIL import ImageFont
+
+from lib.text_writer import get_font_with_auto_adjusted_size
+
+
+class TextWriterTest(TestCase):
+    def test_human_data_generator(self):
+        font_path = "/home/wo/Workspace/zdennis/yearbookers/tests/assets/fonts/gabiant-regular.ttf"
+        starting_size = 50
+        font = ImageFont.truetype(font_path, starting_size)
+        # (x-start, y-start, x-end, y-end)
+        boundaries = (0, 0, 10, 15)
+
+        text_hit_y = "A"
+        font_hit_y = get_font_with_auto_adjusted_size(font, starting_size, boundaries, text_hit_y)
+
+        self.assertEqual(font_hit_y.size, 14)
+
+        text_hit_x = "Aaaaaaaa"
+        font_hit_x = get_font_with_auto_adjusted_size(font, starting_size, boundaries, text_hit_x)
+
+        self.assertEqual(font_hit_x.size, 2)
+
