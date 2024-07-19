d7a3d730040bc4f960584e2e188927583ca57dea
dennis lesmana <dennis.lesmana@vidio.com>
2024-07-19 19:38:15 +0700

    ref page

--- yearbook_generator.py
+++ yearbook_generator.py
@@ -6,6 +6,7 @@ from lib.text_writer import get_font_with_auto_adjusted_size
 
 font_path_default = "assets/fonts/gabiant-regular.ttf"
 
+
 def generate_human_card_sections(image, text, margin_top, font_size, boundaries_y_start, boundaries_y_end=None, font_path=font_path_default):
 	boundaries_x_start = 39
 	boundaries_x_end = 739
@@ -29,10 +30,11 @@ def generate_human_card(single_human):
 	background = cv2.imread('assets/images/human_card_background.jpg')
 	profile_picture = cv2.imread(f'assets/images/profile_pictures/{single_human.imagename}')
 
+	background = cv2.resize(background, (777, 1649))
 	profile_picture = cv2.resize(profile_picture, (700, 700))
 	background[39:739, 39:739] = profile_picture
 
-	card_filename = f'card_{single_human.name}.jpg'
+	card_filename = f'assets/images/cards/card_{single_human.name}.jpg'
 	cv2.imwrite(card_filename, background)
 
 	# Open an Image
@@ -93,8 +95,50 @@ def generate_human_card(single_human):
 	# Save the edited image
 	img.save(card_filename)
 
+
+def generate_page(humans):
+	background = cv2.imread('assets/images/background_test2.jpg')
+	print(humans[0].imagename)
+	card1 = cv2.imread(f'assets/images/cards/card_{humans[0].imagename}')
+	card2 = cv2.imread(f'assets/images/cards/card_{humans[1].imagename}')
+	card3 = cv2.imread(f'assets/images/cards/card_{humans[2].imagename}')
+	card4 = cv2.imread(f'assets/images/cards/card_{humans[3].imagename}')
+	card5 = cv2.imread(f'assets/images/cards/card_{humans[4].imagename}')
+	card6 = cv2.imread(f'assets/images/cards/card_{humans[5].imagename}')
+
+	top_margin = 105
+	card_size_y = 1649
+	row1_end = top_margin + card_size_y
+	row2_end = row1_end + card_size_y
+
+	left_margin = 74
+	card_size_x = 777
+	col1_end = left_margin + card_size_x
+	col2_end = col1_end + card_size_x
+	col3_end = col2_end + card_size_x
+
+	background[top_margin:row1_end, left_margin:col1_end] = card1
+	background[top_margin:row1_end, col1_end:col2_end] = card2
+	background[top_margin:row1_end, col2_end:col3_end] = card3
+	background[row1_end:row2_end, left_margin:col1_end] = card4
+	background[row1_end:row2_end, col1_end:col2_end] = card5
+	background[row1_end:row2_end, col2_end:col3_end] = card6
+
+	card_filename = f'assets/images/pages/page.jpg'
+	cv2.imwrite(card_filename, background)
+
+	# # Open an Image
+	# img = Image.open(card_filename)
+	#
+	# # Call draw Method to add 2D graphics in an image
+	# image = ImageDraw.Draw(img)
+
+
+
 human_lists = generate_human_data("assets/csv_data/sample.csv")
+
 for human in human_lists:
 	print(human.name)
-	print(human.imagename)
 	generate_human_card(human)
+
+generate_page(human_lists)
